76fa0be2d899cff2e298e508204a411263a99528
dasnie97 <damiansnieda97@gmail.com>
2023-01-01 18:46:39 +0100

    Added base class for TestStep.

---
+++ ProductTest/Common/TestStepBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductTest.Common;
+
+public abstract class TestStepBase
+{
+    public string TestName { get; protected set; }
+    public string TestType { get; protected set; }
+    public DateTime TestDateTimeFinish { get; protected set; }
+    public string TestStatus { get; protected set; }
+    public string TestValue { get; protected set; }
+    public string ValueUnit { get; protected set; }
+    public string TestLowerLimit { get; protected set; }
+    public string TestUpperLimit { get; protected set; }
+    public bool IsNumeric { get; protected set; }
+    public string Failure { get; protected set; }
+}
