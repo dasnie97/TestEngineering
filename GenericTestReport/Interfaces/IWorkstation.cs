2ba8757fab0c7ce30207bfffa03e288372038a6a
dasnie97 <damiansnieda97@gmail.com>
2023-01-01 19:09:12 +0100

    Renamed solution file, deleted old files

--- GenericTestReport/Interfaces/IWorkstation.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GenericTestReport.Interfaces
-{
-    public interface IWorkstation
-    {
-        public string Name { get; set; }
-        public string Customer { get; set; }
-        public string State { get; set; }
-    }
-}
