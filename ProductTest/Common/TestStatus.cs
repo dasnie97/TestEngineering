73d29accbd10dffdf245f98cb3e0f4607bc053fd
dasnie97 <damiansnieda97@gmail.com>
2023-01-01 18:46:19 +0100

    Added TestStatus representing test status.

---
+++ ProductTest/Common/TestStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductTest.Common;
+
+public class TestStatus
+{
+    public static readonly TestStatus Passed = Passed;
+    public static readonly TestStatus Failed = Failed;
+    public static readonly TestStatus Terminated = Terminated;
+    public static readonly TestStatus Error = Error;
+}
