b9ee09b7aec121dbab7b6b1b66d24f2fd24fcb3f
dasnie97 <damiansnieda97@gmail.com>
2023-01-04 19:05:56 +0100

    Improved properties names

--- ProductTest/Common/TestStepBase.cs
+++ ProductTest/Common/TestStepBase.cs
@@ -1,21 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ProductTest.Common;
+﻿namespace ProductTest.Common;
 
 public abstract class TestStepBase
 {
-    public string TestName { get; protected set; }
-    public string TestType { get; protected set; }
-    public DateTime TestDateTimeFinish { get; protected set; }
-    public string TestStatus { get; protected set; }
-    public string TestValue { get; protected set; }
-    public string ValueUnit { get; protected set; }
-    public string TestLowerLimit { get; protected set; }
-    public string TestUpperLimit { get; protected set; }
+    public string Name { get; protected set; }
+    public string Type { get; protected set; }
+    public DateTime DateTimeFinish { get; protected set; }
+    public string Status { get; protected set; }
+    public string Value { get; protected set; }
+    public string Unit { get; protected set; }
+    public string LowerLimit { get; protected set; }
+    public string UpperLimit { get; protected set; }
     public bool IsNumeric { get; protected set; }
     public string Failure { get; protected set; }
+
+    protected TestStepBase(string name,
+                            DateTime dateTimeFinish,
+                            string status,
+                            string type = "",
+                            string value = "",
+                            string unit = "",
+                            string lowerLimit = "",
+                            string upperLimit = "",
+                            string failure = "")
+    {
+        Name = name;
+        DateTimeFinish = dateTimeFinish;
+        Status = status;
+        Type = type;
+        Value = value;
+        Unit = unit;
+        LowerLimit = lowerLimit;
+        UpperLimit = upperLimit;
+        Failure = failure;
+        IsNumeric = CheckIfNumeric();
+    }
+    private bool CheckIfNumeric()
+    {
+        return float.TryParse(Value, out _) && (LowerLimit != UpperLimit);
+    }
 }
