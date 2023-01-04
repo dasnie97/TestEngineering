9a6348aae791cc9caaebc9c281024930380a760e
dasnie97 <damiansnieda97@gmail.com>
2023-01-04 19:05:16 +0100

    Added protected constructor

--- ProductTest/Common/TestReportBase.cs
+++ ProductTest/Common/TestReportBase.cs
@@ -3,7 +3,7 @@
 public abstract class TestReportBase
 {
     public string SerialNumber { get; protected set; }
-    public TestStatus Status { get; protected set; }
+    public string Status { get; protected set; }
     public Workstation Workstation { get; protected set; }
     public IEnumerable<TestStepBase> TestSteps { get; protected set; }
     public DateTime TestDateTimeStarted { get; protected set; }
@@ -13,4 +13,23 @@ public abstract class TestReportBase
     //public bool IsFirstPass { get; set; }
     //public bool FalseCall { get; set; }
 
+    protected TestReportBase(string serialNumber,
+                            string status,      
+                            string workstation,
+                            DateTime testStarted,
+                            IEnumerable<TestStep> testSteps,
+                            string failure = "",
+                            string fixtureSocket = "",
+                            TimeSpan? testingTime = null
+    )
+    {
+        SerialNumber = serialNumber;
+        Status = status;
+        Workstation = new Workstation(workstation);
+        TestDateTimeStarted = testStarted;
+        TestSteps = testSteps;
+        Failure = failure;
+        FixtureSocket = fixtureSocket;
+        TestingTime = testingTime;
+    }
 }
