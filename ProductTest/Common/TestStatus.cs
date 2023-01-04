6770372c64e1806e243509674b304faf5d55348a
dasnie97 <damiansnieda97@gmail.com>
2023-01-04 19:05:33 +0100

    Fixed data types

--- ProductTest/Common/TestStatus.cs
+++ ProductTest/Common/TestStatus.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ProductTest.Common;
+﻿namespace ProductTest.Common;
 
 public class TestStatus
 {
-    public static readonly TestStatus Passed = Passed;
-    public static readonly TestStatus Failed = Failed;
-    public static readonly TestStatus Terminated = Terminated;
-    public static readonly TestStatus Error = Error;
+    public static readonly string Passed = "Passed";
+    public static readonly string Failed = "Failed";
+    public static readonly string Terminated = "Terminated";
+    public static readonly string Error = "Error";
 }
