ad393ee0e55d537b887875d6304fde4453f2be0e
dasnie97 <damiansnieda97@gmail.com>
2023-01-14 19:17:07 +0100

    TestSteps are now of type IEnumerable, deleted optional params in ctor, moved common methods to calculate Failure, FixtureSocket and TestingTime.

--- ProductTest/Common/TestReportBase.cs
+++ ProductTest/Common/TestReportBase.cs
@@ -7,7 +7,7 @@ public abstract class TestReportBase
     public string SerialNumber { get; protected set; }
     public string Status { get; protected set; }
     public WorkstationBase Workstation { get; protected set; }
-    public List<TestStepBase> TestSteps { get; protected set; }
+    public IEnumerable<TestStepBase> TestSteps { get; protected set; }
     public DateTime TestDateTimeStarted { get; protected set; }
     public string Failure { get; protected set; }
     public string? FixtureSocket { get; protected set; }
@@ -16,10 +16,7 @@ public abstract class TestReportBase
                             string status,      
                             string workstation,
                             DateTime testStarted,
-                            List<TestStepBase> testSteps,
-                            string failure = "",
-                            string fixtureSocket = "",
-                            TimeSpan? testingTime = null
+                            List<TestStepBase> testSteps
     )
     {
         SerialNumber = serialNumber;
@@ -27,8 +24,54 @@ public abstract class TestReportBase
         Workstation = new Workstation(workstation);
         TestDateTimeStarted = testStarted;
         TestSteps = testSteps;
-        Failure = failure;
-        FixtureSocket = fixtureSocket;
-        TestingTime = testingTime;
+        SetFailedStepData();
+        SetTestSocket();
+        SetBoardTestingTime();
+    }
+    protected TestReportBase()
+    {
+        SerialNumber = string.Empty;
+        Status = string.Empty;
+        Workstation = new Workstation("Default");
+        TestDateTimeStarted = DateTime.Now;
+        TestSteps = new List<TestStepBase>();
+    }
+
+    protected virtual void SetFailedStepData()
+    {
+        var failDetails = "";
+        foreach (var test in TestSteps)
+        {
+            if (test.Status.Contains("fail", StringComparison.OrdinalIgnoreCase))
+                failDetails = $"{test.Name}\nValue measured: {test.Value}\nLower limit: {test.LowerLimit}\nUpper limit: {test.UpperLimit}";
+        }
+        Failure = failDetails;
+    }
+
+    protected virtual void SetBoardTestingTime()
+    {
+        try
+        {
+            var minTime = TestSteps.Min(testStep => testStep.DateTimeFinish);
+            var maxTime = TestSteps.Max(testStep => testStep.DateTimeFinish);
+            TestingTime = maxTime - minTime;
+        }
+        catch
+        {
+            TestingTime = null;
+        }
+    }
+
+    protected virtual void SetTestSocket()
+    {
+        var socketNumberTest = TestSteps.Where(testStep => testStep.Name == "Test Socket Number");
+        if (socketNumberTest.Any())
+        {
+            FixtureSocket = socketNumberTest.First().Value;
+        }
+        else
+        {
+            FixtureSocket = "";
+        }
     }
 }
