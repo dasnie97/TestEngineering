34c2eabde8da2e8c5ec983e75a436cd84d9f1ce0
dasnie97 <damiansnieda97@gmail.com>
2023-05-01 10:26:25 +0200

    Added converter ToTestReportDTO. Added TestReportGenerator using Bogus

--- TestEngineering/DTO/DTOConverter.cs
+++ TestEngineering/DTO/DTOConverter.cs
@@ -30,4 +30,19 @@ public static class DTOConverter
 
         return dto;
     }
+
+    public static TestReportDTO ToTestReportDTO(TestReport source)
+    {
+        TestReportDTO dto = new TestReportDTO();
+
+        dto.Workstation = source.Workstation.Name;
+        dto.SerialNumber = source.SerialNumber;
+        dto.Status = source.Status.ToString();
+        dto.TestDateTimeStarted = source.TestDateTimeStarted;
+        dto.TestingTime = source.TestingTime;
+        dto.FixtureSocket = source.FixtureSocket;
+        dto.Failure = source.Failure;
+
+        return dto;
+    }
 }
---
+++ TestEngineering/Other/TestReportGenerator.cs
@@ -0,0 +1,19 @@
+﻿using AutoBogus;
+using Bogus;
+using System.Security.Cryptography.X509Certificates;
+using TestEngineering.Models;
+
+namespace TestEngineering.Other;
+
+public class TestReportGenerator
+{
+	public TestReportGenerator()
+	{
+		Randomizer.Seed = new Random(1);
+	}
+
+	public static TestReport GenerateFakeTestReport()
+	{
+		return AutoFaker.Generate<TestReport>();
+    }
+}
