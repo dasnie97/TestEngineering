64fd3162069e67ceda1182f0f346fde6fbeabf90
dasnie97 <damiansnieda97@gmail.com>
2023-05-14 16:29:09 +0200

    Moved file processing logic from TestManager

---
+++ TestEngineering/Interfaces/IFileLoader.cs
@@ -0,0 +1,9 @@
+﻿using TestEngineering.Models;
+
+namespace TestEngineering.Interfaces;
+
+public interface IFileLoader
+{
+    IEnumerable<string> GetFiles(string inputDirectoryPath);
+    IEnumerable<FileTestReport> GetTestReportFiles(string inputDirectoryPath);
+}
---
+++ TestEngineering/Interfaces/IFileProcessor.cs
@@ -0,0 +1,11 @@
+﻿using TestEngineering.Models;
+
+namespace TestEngineering.Interfaces;
+
+public interface IFileProcessor
+{
+    public IEnumerable<FileTestReport> LoadFiles();
+    public string MoveFile(FileTestReport testReport);
+    public string CopyFile(FileTestReport testReport);
+    public void DeleteFile(FileTestReport testReport);
+}
