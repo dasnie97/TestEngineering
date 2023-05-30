3bd5e5ee74e3ce4405bfbd88e865577e25659542
dasnie97 <damiansnieda97@gmail.com>
2023-05-30 21:39:45 +0200

    State nullable

--- TestEngineering/DTO/WorkstationDTO.cs
+++ TestEngineering/DTO/WorkstationDTO.cs
@@ -5,5 +5,5 @@ public class WorkstationDTO
     public string Name { get; set; }
     public string OperatorName { get; set; }
     public string ProcessStep { get; set; }
-    public string State { get; set; }
+    public string? State { get; set; }
 }
